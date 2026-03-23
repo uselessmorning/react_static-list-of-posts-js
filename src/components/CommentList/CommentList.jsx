3b2e80e206a20ced5e5d3b2898fd82752717f074
uselessmorning <shtoykoigor@ukr.net>
2026-03-23 18:03:52 +0100

    new solution

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,11 +1,9 @@
 import { CommentInfo } from '../CommentInfo/CommentInfo';
 
 export const CommentList = ({ comments }) => (
-  <>
-    <div className="CommentList">
-      {comments.map(comment => (
-        <CommentInfo key={comment.id} comment={comment} />
-      ))}
-    </div>
-  </>
+  <div className="CommentList">
+    {comments.map(comment => (
+      <CommentInfo key={comment.id} comment={comment} />
+    ))}
+  </div>
 );
--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,16 +2,14 @@ import { CommentList } from '../CommentList/CommentList';
 import { UserInfo } from '../UserInfo/UserInfo';
 
 export const PostInfo = ({ post }) => (
-  <>
-    <div className="PostInfo">
-      <h3 className="PostInfo__title">{post.title}</h3>
-      <UserInfo user={post.user} />
-      <p className="PostInfo__body">{post.body}</p>
-      {post.comments.length > 0 ? (
-        <CommentList comments={post.comments} />
-      ) : (
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      )}
-    </div>
-  </>
+  <div className="PostInfo">
+    <h3 className="PostInfo__title">{post.title}</h3>
+    <UserInfo user={post.user} />
+    <p className="PostInfo__body">{post.body}</p>
+    {post.comments.length > 0 ? (
+      <CommentList comments={post.comments} />
+    ) : (
+      <b data-cy="NoCommentsMessage">No comments yet</b>
+    )}
+  </div>
 );
